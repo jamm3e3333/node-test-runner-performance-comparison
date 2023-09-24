711308ae3019d03aeab4d0c257369fc9cb955eb4
jakub_vala <jakub.vala@betsys.com>
2023-09-24 19:50:15 +0200

    Finalize

--- promise.ts
+++ promise.ts
@@ -1,12 +1,3 @@
-export const testBody = () => [
-        
-        // import lot of modules
-        require('path').join(__dirname, __filename),
-        require('crypto').createHmac('md5', '').update(__filename).digest('hex'),
-        require('os').userInfo(),
-        require('fs').stat('', () => {}),
-    
-]
-//  new Promise(
-//     resolve => setTimeout(resolve, Number(process.env.TEST_WAIT ?? 0))
-// );
+export const testBody = () => new Promise(
+    resolve => setTimeout(resolve, Number(process.env.TEST_WAIT ?? 0))
+);
