13312cc6cc5ec62d588655e3df2ef866e3651e36
jakub_vala <jakub.vala@betsys.com>
2023-09-24 19:49:47 +0200

    Add module imports

--- promise.ts
+++ promise.ts
@@ -1,3 +1,7 @@
-export const testBody = () => new Promise(
-    resolve => setTimeout(resolve, Number(process.env.TEST_WAIT ?? 0))
-);
+export const testBody = async () => [
+        (await import('path')).join(__dirname, __filename),
+        (await import('crypto')).createHmac('md5', '').update(__filename).digest('hex'),
+        (await import('os')).userInfo(),
+        (await import('fs')).stat('', () => {}),
+    
+]
